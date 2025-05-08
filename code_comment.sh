5f4bfb875ad513110fdfa6853ebebe4100317391
wongzhenhao <zhenhao1141@gmail.com>
2025-05-08 13:29:21 +0800

    rm others

--- code_comment.sh
+++
@@ -1,5 +0,0 @@
-# env
-export HF_ENDPOINT=https://hf-mirror.com
-export PATH=$PATH:/root/workspace/culfjk4p420c73amv510/herunming/DataFlow
-
-python code_comment.py --yaml_path CodePipeline/yaml/code_comment.yaml
