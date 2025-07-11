e3e7884f9e2d10c70b0d29b6ff4dad5843202324
zzy1127 <1726073424@qq.com>
2025-07-11 17:22:24 +0800

    fix bug for init

--- dataflow/operators/generate/GeneralText/__init__.py
+++ dataflow/operators/generate/GeneralText/__init__.py
@@ -2,6 +2,6 @@ from .pretrain_generator import PretrainGenerator
 from .sft_generator import SupervisedFinetuneGenerator
 
 __all__ = [
-    "pretrain_generator",
-    "sft_generator",
+    "PretrainGenerator",
+    "SupervisedFinetuneGenerator",
 ]
