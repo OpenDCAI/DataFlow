2881049668ec4f7f3ba691181862177f8ecf2a30
Hao Liang <hao.liang@stu.pku.edu.cn>
2025-08-29 23:54:55 +0800

    import APIVLMServing_openai

--- dataflow/serving/__init__.py
+++ dataflow/serving/__init__.py
@@ -1,6 +1,7 @@
 from .api_llm_serving_request import APILLMServing_request
 from .local_model_llm_serving import LocalModelLLMServing_vllm
 from .local_model_llm_serving import LocalModelLLMServing_sglang
+from .api_vlm_serving_openai import APIVLMServing_openai
 from .google_api_serving import PerspectiveAPIServing
 from .lite_llm_serving import LiteLLMServing
 
@@ -11,6 +12,7 @@ __all__ = [
     "APILLMServing_request",
     "LocalModelLLMServing_vllm",
     "LocalModelLLMServing_sglang",
+    "APIVLMServing_openai",
     "PerspectiveAPIServing",
     "LiteLLMServing",
     "LocalModelLALMServing_vllm"
