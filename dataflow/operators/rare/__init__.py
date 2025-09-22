eecac0c90c7add15fb917d26f3395a2036d755d8
Rise-1210 <risemds@163.com>
2025-09-22 16:46:48 +0800

    [Refactor] RARE pipeline with fixed bugs

--- dataflow/operators/rare/__init__.py
+++ dataflow/operators/rare/__init__.py
@@ -14,6 +14,6 @@ else:
 
     cur_path = "dataflow/operators/rare/"
 
-    print(_import_structure)
+    #print(_import_structure)
     _import_structure = generate_import_structure_from_type_checking(__file__, cur_path)
     sys.modules[__name__] = LazyLoader(__name__, "dataflow/operators/rare/", _import_structure)
